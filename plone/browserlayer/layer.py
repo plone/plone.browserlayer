c07328c705326194afb0463785578f5cbe4dd2e8
Martin Aspeli <optilude@gmail.com>
2007-10-08 21:33:18 +0000

    We need to prepend, not append the interface to properly override things defined for the default skin
    svn path=/plone.browserlayer/trunk/; revision=17481

--- plone/browserlayer/layer.py
+++ plone/browserlayer/layer.py
@@ -1,10 +1,18 @@
-from zope.interface import alsoProvides
+from zope.interface import directlyProvidedBy, directlyProvides
 from zope.component import getAllUtilitiesRegisteredFor
 from plone.browserlayer.interfaces import ILocalBrowserLayerType
 
 def mark_layer(site, event):
     """Mark the request with all installed layers.
     """
-    
+    request = event.request
     for layer in getAllUtilitiesRegisteredFor(ILocalBrowserLayerType):
-        alsoProvides(event.request, layer)
+        
+        # Since we allow multiple markers here, we can't use 
+        # zope.publisher.browser.applySkin() since this filters out 
+        # IBrowserSkinType interfaces, nor can we use alsoProvides(), since
+        # this appends the interface (in which case we end up *after* the
+        # default Plone/CMF skin)
+
+        ifaces = [layer] + list(directlyProvidedBy(request))
+        directlyProvides(request, *ifaces)
