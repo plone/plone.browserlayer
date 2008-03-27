ae60ddf68c90ea2513ef86a61202f803aa85f8d4
Andreas Zeidler <witsch@plone.org>
2008-03-27 21:45:41 +0000

    slight performance optimization as discussed during [source:review/plip220-browserlayer/REVIEW-NOTES.txt#19168 during bundle review] for [http://plone.org/products/plone/roadmap/220 plip220] (also see #7754)
    svn path=/plone.browserlayer/trunk/; revision=20105

--- plone/browserlayer/layer.py
+++ plone/browserlayer/layer.py
@@ -6,13 +6,14 @@ def mark_layer(site, event):
     """Mark the request with all installed layers.
     """
     request = event.request
-    for layer in getAllUtilitiesRegisteredFor(ILocalBrowserLayerType):
+    layers = getAllUtilitiesRegisteredFor(ILocalBrowserLayerType)
+    ifaces = list(layers) + list(directlyProvidedBy(request))
         
-        # Since we allow multiple markers here, we can't use 
-        # zope.publisher.browser.applySkin() since this filters out 
-        # IBrowserSkinType interfaces, nor can we use alsoProvides(), since
-        # this appends the interface (in which case we end up *after* the
-        # default Plone/CMF skin)
+    # Since we allow multiple markers here, we can't use 
+    # zope.publisher.browser.applySkin() since this filters out 
+    # IBrowserSkinType interfaces, nor can we use alsoProvides(), since
+    # this appends the interface (in which case we end up *after* the
+    # default Plone/CMF skin)
 
-        ifaces = [layer] + list(directlyProvidedBy(request))
-        directlyProvides(request, *ifaces)
+    directlyProvides(request, *ifaces)
+
