43d084c40265eac78b0d1151012ad47a6c7dc2e5
Maurits van Rees <maurits@vanrees.org>
2020-03-28 01:25:51 +0100

    Added Python 3.8 to classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     keywords='plone browser layer',
     author='Plone Foundation',
