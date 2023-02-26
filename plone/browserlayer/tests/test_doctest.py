ec94bfeca947eeb727cc02398500b4b7a8fac065
Gil Forcada Codinachs <gil.gnome@gmail.com>
2023-02-26 23:52:00 +0100

    fix: remove python 2 compatiblity code

--- plone/browserlayer/tests/test_doctest.py
+++ plone/browserlayer/tests/test_doctest.py
@@ -2,16 +2,7 @@ from plone.browserlayer.testing import PLONEBROWSERLAYER_FUNCTIONAL_TESTING
 from plone.testing import layered
 
 import doctest
-import re
-import six
 import unittest
-
-
-class Py23DocChecker(doctest.OutputChecker):
-    def check_output(self, want, got, optionflags):
-        if six.PY2:
-            want = re.sub("urllib.error.HTTPError", "HTTPError", want)
-        return doctest.OutputChecker.check_output(self, want, got, optionflags)
 
 
 def test_suite():
@@ -26,7 +17,6 @@ def test_suite():
                         | doctest.REPORT_ONLY_FIRST_FAILURE
                         | doctest.NORMALIZE_WHITESPACE
                     ),
-                    checker=Py23DocChecker(),
                 ),
                 layer=PLONEBROWSERLAYER_FUNCTIONAL_TESTING,
             )
