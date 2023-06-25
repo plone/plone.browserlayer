3285528702cf4a4b80ce78759f3fe03d6ca6c432
Gil Forcada Codinachs <gil.gnome@gmail.com>
2023-06-25 23:15:05 +0200

    fix: adjust setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
+from pathlib import Path
 from setuptools import find_packages
 from setuptools import setup
 
 
 version = "3.0.2.dev0"
 
+long_description = (
+    f"{Path('README.rst').read_text()}\n{Path('CHANGES.rst').read_text()}"
+)
+
 setup(
     name="plone.browserlayer",
     version=version,
     description="Browser layer management for Zope applications",
-    long_description=(open("README.rst").read() + "\n" + open("CHANGES.rst").read()),
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    # Get more strings from
+    # https://pypi.org/classifiers/
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
